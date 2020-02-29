793dbfc93296eb9af07e95b859ba67d4967651e4
Lars Baumgaertner <1264131+gh0st42@users.noreply.github.com>
2020-02-29 18:56:30 +0100

    Refactored to use psutil crate for macos support

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,9 @@
 
 use clap::{crate_authors, crate_version, Clap};
 use ctrlc;
-use procfs::process::Process;
-use procfs::process::Stat;
-use procfs::KernelStats;
-use std::convert::TryInto;
+use psutil::process::Process;
 use std::fmt;
+use std::io::Result;
 use std::io::{self, Write};
 use std::process::Command;
 use std::sync::atomic::{AtomicBool, Ordering};
@@ -41,7 +39,7 @@ struct Opts {
     duration: Option<u64>,
     /// Process to be inspected
     #[clap(short = "p", long = "pid")]
-    pid: i32,
+    pid: u32,
     /// A level of verbosity, and can be used multiple times
     #[clap(short = "v", long = "verbose", parse(from_occurrences))]
     verbose: i32,
@@ -54,50 +52,60 @@ struct Opts {
 #[derive(Debug)]
 struct Sample {
     ts: f32,
-    pid: i32,
-    num_threads: i64,
+    pid: u32,
+    //num_threads: u64,
     cpu: f32,
     vsize: u64,
-    rss: i64,
+    rss: u64,
 }
 
 impl fmt::Display for Sample {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
-            "{:.02} PID {} CPU% {:.02} RSS {} VSIZE {} THREADS {}",
-            self.ts, self.pid, self.cpu, self.rss, self.vsize, self.num_threads
+            "{:.02} PID {} CPU% {:.02} RSS {} VSIZE {} ",
+            self.ts, self.pid, self.cpu, self.rss, self.vsize
         )
     }
 }
 
-fn get_total_jiffies() -> f32 {
-    let cpu = KernelStats::new().unwrap().total;
-    cpu.user
-        + cpu.nice
-        + cpu.system
-        + cpu.idle
-        + cpu.iowait.unwrap_or_default()
-        + cpu.irq.unwrap_or_default()
-        + cpu.steal.unwrap_or_default()
-        + cpu.softirq.unwrap_or_default()
-}
-fn get_stat(pid: i32) -> Stat {
-    let target = Process::new(pid).unwrap();
-    target.stat().unwrap()
-}
 fn delay(millis: u64) {
     let timeout = time::Duration::from_millis(millis);
     thread::sleep(timeout);
 }
+fn gnuplot_recording(recording: &[Sample]) -> Result<()> {
+    let gnuplot_script_content = include_str!("../recording.plot");
+    let mut gnuplot_file = NamedTempFile::new()?;
+    gnuplot_file.write_all(gnuplot_script_content.as_bytes())?;
+
+    let mut data_file = NamedTempFile::new()?;
+    for i in recording {
+        data_file.write_all(format!("{}\n", i).as_bytes())?;
+    }
+    data_file.flush()?;
+    let fname_param = format!("filename={:?};", data_file.path().display());
+
+    let output = Command::new("gnuplot")
+        .arg("-e")
+        .arg(fname_param)
+        .arg("-p")
+        .arg(gnuplot_file.path())
+        .output()?;
+
+    if !output.status.success() {
+        println!("status: {}", output.status);
+        io::stdout().write_all(&output.stdout)?;
+        io::stderr().write_all(&output.stderr)?;
+    }
+    Ok(())
+}
 fn main() {
     let opts: Opts = Opts::parse();
 
-    let cores = procfs::CpuInfo::new().unwrap().num_cores() as f32;
+    // SETUP phase
+    let mut pid_proc = Process::new(opts.pid).expect("Failed getting process");
+    let _percent_cpu = pid_proc.cpu_percent();
     let sample_rate = opts.interval * 1000;
-    let mut jiffies1 = get_total_jiffies() / cores;
-    let stats = get_stat(opts.pid);
-    let mut lasttimes = stats.utime + stats.stime;
 
     let mut recording = vec![];
     let mut start: Option<SystemTime> = None;
@@ -108,28 +116,24 @@ fn main() {
         r.store(false, Ordering::SeqCst);
     })
     .expect("Error setting Ctrl-C handler");
+    // MAIN phase
     while running.load(Ordering::SeqCst) {
         delay(sample_rate);
-        let jiffies2 = get_total_jiffies() / cores;
-        let stats = get_stat(opts.pid);
-        let d_j = jiffies2 - jiffies1;
-        jiffies1 = jiffies2;
-        let d_t: f32 = (stats.utime + stats.stime - lasttimes) as f32;
-        let percent_cpu = d_t / d_j as f32;
-        let time_since_start = if start.is_none() {
+        let percent_cpu = pid_proc.cpu_percent().unwrap();
+        let cur_mem = pid_proc.memory_info().unwrap();
+        let time_since_start = if let Some(time) = start {
+            time.elapsed().unwrap().as_secs_f32()
+        } else {
             start = Some(time::SystemTime::now());
             0.0
-        } else {
-            start.unwrap().elapsed().unwrap().as_secs_f32()
         };
-
         let data = Sample {
             ts: time_since_start,
-            pid: opts.pid,
+            pid: pid_proc.pid(),
             cpu: percent_cpu,
-            rss: stats.rss,
-            vsize: stats.vsize,
-            num_threads: stats.num_threads,
+            rss: cur_mem.rss() / 1000,
+            vsize: cur_mem.vms() / 1000,
+            //num_threads: pid_proc.num_threads(),
         };
         if opts.verbose > 0 {
             println!("{}", data);
@@ -140,38 +144,17 @@ fn main() {
                 break;
             }
         }
-        lasttimes = stats.utime + stats.stime;
     }
 
+    // POST phase
     if opts.verbose == 0 {
         for i in &recording {
             println!("{}", i);
         }
     }
     if opts.graph {
-        let gnuplot_script_content = include_str!("../recording.plot");
-        let mut gnuplot_file = NamedTempFile::new().unwrap();
-        gnuplot_file.write_all(gnuplot_script_content.as_bytes());
-
-        let mut data_file = NamedTempFile::new().unwrap();
-        for i in &recording {
-            data_file.write_all(format!("{}\n", i).as_bytes());
-        }
-        data_file.flush();
-        let fname_param = format!("filename={:?};", data_file.path().display());
-
-        let output = Command::new("gnuplot")
-            .arg("-e")
-            .arg(fname_param)
-            .arg("-p")
-            .arg(gnuplot_file.path())
-            .output()
-            .expect("failed to execute process");
-
-        if !output.status.success() {
-            println!("status: {}", output.status);
-            io::stdout().write_all(&output.stdout).unwrap();
-            io::stderr().write_all(&output.stderr).unwrap();
+        if let Err(err) = gnuplot_recording(&recording) {
+            println!("Fatal error calling gnuplot: {}", err);
         }
     }
 }
